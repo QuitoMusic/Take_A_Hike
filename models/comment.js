46fa7b1883723c36dbe75f3bf27d14a93f47ffa5
Lexxvasquez <122750837+Lexxvasquez@users.noreply.github.com>
2023-07-27 16:19:01 -0400

    final comments

--- models/comment.js
+++ models/comment.js
@@ -18,3 +18,28 @@ Comment.init(
               len: [1]
           }
       },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user', 
+            key: 'id' 
+        }
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'post', 
+            key: 'id' 
+        }
+    }
+},
+{
+    sequelize,
+    freezeTableName: true, 
+    underscored: true, 
+    modelName: 'comment' 
+}
+);
+
+
+module.exports = Comment;
