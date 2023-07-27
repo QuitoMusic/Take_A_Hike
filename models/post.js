97e6011d263a0bb1e6cf8ed1d8dcd54f1dfe3627
LarryByrd89 <laryn.n99@gmail.com>
2023-07-27 16:08:28 -0400

    post.js2

--- models/post.js
+++ models/post.js
@@ -3,3 +3,13 @@ const sequelize = require('../config/connection');
 
 
 class Post extends Model {}
+
+// Initialize the Post model with its attributes and options
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
