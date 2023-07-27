4c76041e6e61f82e760f9ac536ab83665ab5a210
Lexxvasquez <122750837+Lexxvasquez@users.noreply.github.com>
2023-07-27 15:57:43 -0400

    import comment

--- models/comment.js
+++ models/comment.js
@@ -2,3 +2,19 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
+
+Comment.init(
+  {
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      comment_text: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              len: [1]
+          }
+      },
