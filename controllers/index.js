2a9c5f41d964f5a25e5585f2053f7564cdfbdb5b
LarryByrd89 <laryn.n99@gmail.com>
2023-07-27 20:09:20 -0400

    consolidate route for homepage

---
+++ controllers/index.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+// Import routes 
+const apiRoutes = require('./apiRoutes');
+const homeRoutes = require('./homeRoutes.js');
+const dashboardRoutes = require('./dashboardRoutes.js');
+
+router.use('/api', apiRoutes);
+// middleware to handle home-related requests
+router.use('/', homeRoutes);
+// middleware to handle dashboard-related requests
+router.use('/dashboard', dashboardRoutes);
+
+// This middleware handles 404 (Not Found) response.
+router.use((req, res) => {
+    res.status(404).end();
+  });
+
+  module.exports = router;
