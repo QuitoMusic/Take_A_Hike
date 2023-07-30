57a98ac4e217f076b41548ca17c138e9cbf30322
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-07-30 16:40:05 -0400

    Fixed seeds scripts on package.json so we can run seeds correctly using  Insomnia, also, ahered seeds to corellate with the hiking topic

--- seeds/postSeeds.js
+++ seeds/postSeeds.js
@@ -2,26 +2,26 @@ const { Post } = require("../models");
 
 const postData = [
   {
-    title: "Coding is so difficult!",
+    title: "Hiking party in the Everglades",
     post_text:
-      "Why is this project so complicated?!",
+      "On August 31st I will be taking a stroll in the Everglades if anyone would like to join me?",
     user_id: 1,
   },
   {
-    title: "I love computers and gaming.",
-    post_text: "Still looking for the perfect console but handhelds are my weakness.",
+    title: "Best scenic hikes in Florida",
+    post_text: "I'm curious about what the best scenic hikes are in Florida. Everyone says the Everglades, but what do you think? ",
     user_id: 3,
   },
   {
-    title: "Instagram",
+    title: "Solo traveler in Florida",
     post_text:
-      "How do I post multiple photos at the same time on Instagram?",
+      "I am new to Florida and would like to meet new people. I am going for a hike at Highlands Hammock State Park soon. Would anyone want to join me?",
     user_id: 2,
   },
   {
-    title: "Vs Code is a masterpiece of a software.",
+    title: "Advanced hiker looking for someone to join.",
     post_text:
-      "Javascript is boss!",
+      "The Myakka Hiking Trail is one of the longest trails in Florida, and I would like to have a partner to join me. Any takers?",
     user_id: 4,
   },
 ];
