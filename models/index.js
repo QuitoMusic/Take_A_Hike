a9eb837b73ca7ca8af51244cdfecd6d24b147a3b
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-08-02 21:41:25 -0400

    trying heroku deployment fix

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Import the necessary models
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
 const Address = require('./addresses');
 
 // Define the associations between the models
